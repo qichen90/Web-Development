ccffe45bae40dd8149d70b7bb578b473f0d5afa6
qichen <chittychen90@gmail.com>
2015-11-21 21:16:51 -0500

    Initial commit

---
+++ restaurant-menu-web/database_setup.py
@@ -0,0 +1,63 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+
+class User(Base):
+  __tablename__ = 'user'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String(250), nullable=False)
+  email = Column(String(250), nullable=False)
+  picture = Column(String(250))
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    menuItems = relationship("MenuItem", cascade="all, delete-orphan")
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+       }
+ 
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'description'         : self.description,
+           'id'         : self.id,
+           'price'         : self.price,
+           'course'         : self.course,
+       }
+
+
+
+engine = create_engine('postgres://cmvbrzkcxbsnfi:NQoPLVRsGtnpiy24WE949_5o8P@ec2-54-83-202-64.compute-1.amazonaws.com:5432/d1mv3vs75f2v0q')
+ 
+
+Base.metadata.create_all(engine)
